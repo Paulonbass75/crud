ea5cc46bee0237557ed7eae1355de5f2cddbb204
Paulonbass75 <paulmpool@yahoo.com>
2023-01-16 13:31:58 -0500

    Laravel Crud with user auth, first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@codingoblin.com',
+                'password' => 'password',
+                'role' => 'admin',
+            ],
+            [
+                'name' => 'John',
+                'email' => 'john@codingoblin.com',
+                'password' => 'password2',
+                'role' => 'standard',
+            ]
+        ];
+
+        foreach ($users as $user) {
+            $created_user = User::create([
+                'name' => $user['name'],
+                'email' => $user['email'],
+                'password' => Hash::make($user['password']),
+            ]);
+
+            $created_user->assignRole($user['role']);
+        }
+    }
+}
